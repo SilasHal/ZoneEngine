4eaead17bdf5e44109b4685b6872b74b0d262eef
CedricZ1001 <Cedric1001Z@gmail.com>
2024-04-15 16:52:41 +0800

    init engine

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
-using namespace std;
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN // 从 Windows 头中排除极少使用的资料
+#include <windows.h>
+#include <tchar.h>
+//添加WTL支持 方便使用COM
+#include <wrl.h>
+using namespace Microsoft;
+using namespace Microsoft::WRL;
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+//for d3d12
+#include <d3d12.h>
+#include <d3d12shader.h>
+#include <d3dcompiler.h>
+//linker
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#if defined(_DEBUG)
+#include <dxgidebug.h>
+#endif
+
+#include "..\WindowsCommons\d3dx12.h"
+
+#define GRS_WND_CLASS_NAME _T("Game Window Class")
+#define GRS_WND_TITLE   _T("DirectX12 Trigger Sample")
+
+#define GRS_THROW_IF_FAILED(hr) if (FAILED(hr)){ throw CGRSCOMException(hr); }
 
 int main() {
-	cout << "Hello Computer Graphics!" <<"\n";
+	std::cout << "Hello Computer Graphics!" <<"\n";
 }
