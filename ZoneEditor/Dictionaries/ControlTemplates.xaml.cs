fc96d620552ef87e4d76ea8e424dbd31ed2b559f
CedricZ1001 <Cedric1001Z@gmail.com>
2024-12-24 18:39:15 +0800

    update TextBox

--- ZoneEditor/Dictionaries/ControlTemplates.xaml.cs
+++ ZoneEditor/Dictionaries/ControlTemplates.xaml.cs
@@ -37,6 +37,43 @@ namespace ZoneEditor.Dictionaries
                 Keyboard.ClearFocus();
             }
         }
+        private void OnTextBoxReaname_keyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+
+            if (e.Key == Key.Enter)
+            {
+                if (textBox.Tag is ICommand command && command.CanExecute(textBox.Text))
+                {
+                    command.Execute(textBox.Text);
+                }
+                else
+                {
+                    exp.UpdateSource();
+                }
+                textBox.Visibility = Visibility.Collapsed;
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                exp.UpdateTarget();
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnTextBoxRename_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if(exp != null)
+            {
+                exp.UpdateTarget();
+                textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
 
         private void OnClose_Button_Click(object sender, RoutedEventArgs e)
         {
