a20589c2c0a05f3456ae188be9933f18ad4c4b6c
CedricZ1001 <Cedric1001Z@gmail.com>
2024-12-29 23:36:47 +0800

    update CreateNewScriptDialog

---
+++ ZoneEditor/GameDev/NewScriptDialog.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ZoneEditor.GameProject;
+
+namespace ZoneEditor.GameDev
+{
+    /// <summary>
+    /// Interaction logic for NewScriptDialog.xaml
+    /// </summary>
+    public partial class NewScriptDialog : Window
+    {
+        public NewScriptDialog()
+        {
+            InitializeComponent();
+        }
+
+        bool Validate()
+        {
+            bool isValid = false;
+            var name = scriptName.Text.Trim();
+            var path = scriptPath.Text.Trim();
+            string errorMsg = string.Empty;
+            if (string.IsNullOrEmpty(name))
+            {
+                errorMsg = "Script name cannot be empty";
+            }
+            else if (name.IndexOfAny(Path.GetInvalidFileNameChars()) != -1 || name.Any(x => char.IsWhiteSpace(x)))
+            {
+                errorMsg = "Invalid character(s) used in script name.";
+            }
+            else if (string.IsNullOrEmpty(path))
+            {
+                errorMsg = "Script path cannot be empty";
+            }
+            else if (path.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                errorMsg = "Invalid character(s) used in script path.";
+            }
+            else if (!Path.GetFullPath(Path.Combine(Project.Current.Path, path)).Contains(Path.Combine(Project.Current.Path, @"GameCode\")))
+            {
+                errorMsg = "Script path must be within the GameCode folder";
+            }
+            else if (File.Exists(Path.GetFullPath(Path.Combine(Path.Combine(Project.Current.Path, path), $"{name}.cpp"))) ||
+                     File.Exists(Path.GetFullPath(Path.Combine(Path.Combine(Project.Current.Path, path), $"{name}.h")))) 
+            {
+                errorMsg = $"script {name} already exists in this folder.";
+            }
+            else
+            {
+                isValid = true;
+            }
+            
+            if(!isValid)
+            {
+                messageTextBlock.Foreground = FindResource("Editor.RedBrush") as Brush;
+            }
+            else
+            {
+                messageTextBlock.Foreground = FindResource("Editor.FontBrush") as Brush;
+            }
+            messageTextBlock.Text = errorMsg;
+            return isValid;
+        }
+
+        private void OnScriptName_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!Validate()) return;
+        }
+
+        private void OnScriptPath_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void OnCreate_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ZoneEditor/GameDev/VisualStudio.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using ZoneEditor.Utilities;
+
+namespace ZoneEditor.GameDev
+{
+	static class VisualStudio
+	{
+		private static EnvDTE80.DTE2 _vsInstance = null;
+		private static readonly string _progID = "VisualStudio.DTE.17.0";
+		[DllImport("ole32.dll")]
+		private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+
+        [DllImport("ole32.dll")]
+        private static extern int GetRunningObjectTable(uint reserved, out IRunningObjectTable pprot);
+        public static void OpenVisualStudio(string solutionPath)
+		{
+			IRunningObjectTable rot = null;
+			IEnumMoniker monikerTable = null;
+			IBindCtx bindCtx = null;
+			try
+			{
+				if (_vsInstance == null) 
+				{
+					//Find the open visual
+					var hResult = GetRunningObjectTable(0, out rot);
+					if (hResult < 0 || rot == null) throw new COMException($"GetRunningObjectTable() returned HRESULT: {hResult:X8}");
+
+					rot.EnumRunning(out monikerTable);
+					monikerTable.Reset();
+
+					hResult = CreateBindCtx(0, out bindCtx);
+                    if (hResult < 0 || bindCtx == null) throw new COMException($"CreateBindCtx() returned HRESULT: {hResult:X8}");
+
+					IMoniker[] currentMoniker = new IMoniker[1];
+					while(monikerTable.Next(1,currentMoniker,IntPtr.Zero) == 0)
+					{
+						string name = string.Empty;
+						currentMoniker[0]?.GetDisplayName(bindCtx, null, out name);
+						if (name.Contains(_progID))
+						{
+							hResult = rot.GetObject(currentMoniker[0], out object obj);
+                            if (hResult < 0 || obj == null) throw new COMException($"Running object table's GetObject() returned HRESULT: {hResult:X8}");
+
+							EnvDTE80.DTE2 dte = obj as EnvDTE80.DTE2;
+							var solutionName = dte.Solution.FullName;
+							if(solutionName == solutionPath)
+							{
+								_vsInstance = dte;
+								break;
+                            }
+                        }
+                    }
+
+                    if (_vsInstance == null)
+					{
+						Type visualStudio = Type.GetTypeFromProgID(_progID, true);
+                        _vsInstance = Activator.CreateInstance(visualStudio) as EnvDTE80.DTE2;
+                    }
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(e.Message);
+				Logger.Log(MessageType.Error, "Failed to open Visual Studio.");
+            }
+			finally
+			{
+                if (monikerTable != null) Marshal.ReleaseComObject(monikerTable);
+                if (rot != null) Marshal.ReleaseComObject(rot);
+                if (bindCtx != null) Marshal.ReleaseComObject(bindCtx);
+            }
+
+        }
+
+        public static void CloseVisualStudio()
+        {
+            if (_vsInstance?.Solution.IsOpen == null)
+            {
+                _vsInstance.ExecuteCommand("File.SaveAll");
+                _vsInstance.Solution.Close(true);
+            }
+			_vsInstance.Quit();
+            _vsInstance = null;
+		}
+	}
+}
+	
+
