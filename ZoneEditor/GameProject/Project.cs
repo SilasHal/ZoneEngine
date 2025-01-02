0ac3d36ce112e5989b8cb4d547941f6ef78847f3
Hal <d2596113477@gmail.com>
2025-01-02 20:32:49 +0800

    fix ProjectTemplates and add UndoRedo Selection.

--- ZoneEditor/GameProject/Project.cs
+++ ZoneEditor/GameProject/Project.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
