2b6e3f1d0be0082e4d034b6a26df5da945c000d4
Hal <d2596113477@gmail.com>
2025-01-17 18:35:09 +0800

    Testing entities system and creating the engine DLL.

--- Engine/Components/Transform.h
+++ Engine/Components/Transform.h
@@ -3,7 +3,6 @@
 
 namespace zone::transform {
 
-DEFINE_TYPED_ID(transform_id);
 
 struct init_info
 {
@@ -12,6 +11,6 @@ struct init_info
 	f32 scale[3]{ 1.f, 1.f, 1.f };
 };
 
-transform_id create_transform(const init_info& info, game_entity::entity_id entity_id);
-void remove_game_entity(transform_id id);
+component create_transform(const init_info& info, game_entity::entity entity);
+void remove_transform(component c);
 }
---
+++ EngineDLL/EngineAPI.cpp
@@ -0,0 +1,7 @@
+#ifndef EDITOR_INTERFACE
+#define EDITOR_INTREFACE extern "C" __declspec(dllexport)
+#endif // !EDITOR_INTERFACE
+
+EDITOR_INTREFACE
+void CreateGameEntity()
+{ }
